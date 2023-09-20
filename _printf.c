ef16449e4e23624c5c07b618756609526cc901b2
chimenemking <chimenemwillis@gmail.com>
2023-09-19 18:59:51 -0700

    check

--- _printf.c
+++ _printf.c
@@ -1,49 +1,89 @@
 #include "main.h"
+
 /**
- * _printf - to print output to stdout
+ * _printf - print output to stdout
  * @format: format string containing conversion specifiers
- * Return: return the number of characters printed
+ * Return: number of characters printed (excluding null byte)
  */
 int _printf(const char *format, ...)
 {
-	int chars_print = 0;
+	int chars_printed = 0;
 	va_list list_arguments;
 
-	va_start(list_arguments, format);
+	if (!format)
+	return (-1);
 
+	va_start(list_arguments, format);
 	while (*format)
 	{
-		if (*format != '%')
+	if (*format != '%')
+	{
+		chars_printed += put_car(*format);
+	}
+	else
+	{
+	format++;
+		if (*format == '\0')
+			break;
+		if (*format == '%')
 		{
-			write(1, format, 1);
-			chars_print++;
-		} else
+			chars_printed += put_car(*format);
+		}
+		else if (*format == 'c')
 		{
-			format++;
-			if (*format == '\0')
-			{
-				break;
-			}
-			if (*format == '%')
-			{
-				write(1, format, 1);
-				chars_print++;
-			} else if (*format == 'c')
-			{
-				char ch = va_arg(list_arguments, int);
-
-				write(1, &ch, 1);
-				chars_print++;
-			} else if (*format == 's')
-			{
-				char *str = va_arg(list_arguments, char*);
-
-				int strln = strlen(str);
-
-				chars_print += write(1, str, strln);
-			}
-		} format++;
+			chars_printed += print_char(list_arguments);
+		}
+		else if (*format == 's')
+		{
+			chars_printed += print_string(list_arguments);
+		}
+		else if (*format == '%')
+		{
+			chars_printed += print_percent();
+		}
+	}
+	format++;
 	}
 	va_end(list_arguments);
-	return (chars_print);
+	return (chars_printed);
+}
+
+/**
+ * print_char - print a character
+ * @list: va_list with the character to print
+ * Return: number of characters printed
+ */
+int print_char(va_list list)
+{
+	char ch = va_arg(list, int);
+
+	return (put_car(ch));
+}
+
+/**
+ * print_string - print a string
+ * @list: va_list with the string to print
+ * Return: number of characters printed
+ */
+int print_string(va_list list)
+{
+	const char *str = va_arg(list, const char *);
+	int chars_printed = 0;
+
+	while (*str)
+	{
+		put_car(*str);
+		str++;
+		chars_printed++;
+	}
+	return (chars_printed);
+}
+
+/**
+ * print_percent - print a percent sign
+ * Return: 1 (for the percent character)
+ */
+int print_percent(void)
+{
+	return (put_car('%'));
 }
--- main.h
+++ main.h
@@ -5,9 +5,15 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 int put_car(char c);
 int put_ss(char *c);
+int print_char(va_list list);
+int print_string(va_list list);
+int print_percent(void);
+
+
 int _printf(const char *format, ...);
 
 #endif
